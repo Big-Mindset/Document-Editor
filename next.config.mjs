724ceed97267d613d51da661bf1fd40dcd2a99df
Big-Mindset <wadoodmemon0@gmail.com>
2025-10-29 03:22:26 +0500

    done

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,18 @@
 const nextConfig = {
     images : {
         domains : ["t4.ftcdn.net","lh3.googleusercontent.com"]
-    }
+    },
+    outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/.prisma/client/*.node'],
+  },
+    
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  
+}
+
+export default nextConfig
